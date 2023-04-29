1125699842ec9201ea84d0a082bb5ddca80d1307
Antraxmin <antraxmin@naver.com>
2023-04-29 12:42:28 +0900

    [#3] feat: 사이드 카테고리 항목 추가

--- src/component/category/SideDrawer.js
+++ src/component/category/SideDrawer.js
@@ -2,5 +2,21 @@ import React from "react";
 import style from "./SideDrawer.module.css";
 
 export default function SideDrawer() {
-  return <div className={style.side_drawer}>hello</div>;
+  return (
+    <>
+      <div className={style.side_drawer}>
+        <div className={style.btn_box}>
+          <img src="../../img/close-btn.png" width={20} />
+        </div>
+        <div className={style.list_box}>
+          <ul>
+            <li>공연시간표</li>
+            <li>푸드트럭</li>
+            <li>동아리 부스</li>
+            <li>축제 지도</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
