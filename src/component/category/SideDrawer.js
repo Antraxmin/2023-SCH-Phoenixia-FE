1f5e70a51c58091b79043aeb9cbbc2bff42c4db0
Antraxmin <antraxmin@naver.com>
2023-04-29 12:52:34 +0900

    [#3] feat: 사이드바 우측 상단 X 버튼 누르면 닫히도록 구현

--- src/component/category/SideDrawer.js
+++ src/component/category/SideDrawer.js
@@ -1,22 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./SideDrawer.module.css";
 
 export default function SideDrawer() {
+  const [sideIsOpen, setSideIsOpen] = useState(true);
   return (
     <>
-      <div className={style.side_drawer}>
-        <div className={style.btn_box}>
-          <img src="../../img/close-btn.png" width={20} />
+      {sideIsOpen && (
+        <div className={style.side_drawer}>
+          <div className={style.btn_box}>
+            <img
+              src="../../img/close-btn.png"
+              width={20}
+              onClick={() => {
+                setSideIsOpen(!sideIsOpen);
+              }}
+            />
+          </div>
+          <div className={style.list_box}>
+            <ul>
+              <li>공연시간표</li>
+              <li>푸드트럭</li>
+              <li>동아리 부스</li>
+              <li>축제 지도</li>
+            </ul>
+          </div>
         </div>
-        <div className={style.list_box}>
-          <ul>
-            <li>공연시간표</li>
-            <li>푸드트럭</li>
-            <li>동아리 부스</li>
-            <li>축제 지도</li>
-          </ul>
-        </div>
-      </div>
+      )}
     </>
   );
 }
