b2bc70c02f9b0ad2cdd615c93f9c47d96f591a33
Antraxmin <antraxmin@naver.com>
2023-05-05 16:07:34 +0900

    feat: add foodtruck list component

--- src/component/FoodTruck/FoodTruckListBox.js
+++ src/component/FoodTruck/FoodTruckListBox.js
@@ -3,7 +3,19 @@ import style from "./FoodTruckListBox.module.css";
 export default function FoodTruckListBox() {
   return (
     <>
-      <div className={style.container}>test</div>
+      <div className={style.container}>
+        <div className={style.content}>
+          <img src="../img/koko.jpg"></img>
+
+          <div className={style.info}>
+            <div className={style.title}>꼬꼬닭꼬치</div>
+            <div className={style.discription}>
+              안녕하세요. 꼬꼬닭꼬치입니다. 저희는 데리야끼 닭꼬치가
+              주력메뉴입니다
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
