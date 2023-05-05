4276d5f20a8de280548006e6eb7fb533cde3bb73
Antraxmin <antraxmin@naver.com>
2023-05-05 17:06:41 +0900

    fix: 배포 시 이미지 깨짐 오류

--- src/component/FoodTruck/FoodTruck.js
+++ src/component/FoodTruck/FoodTruck.js
@@ -17,13 +17,13 @@ export default function FoodTruck() {
     {
       name: "인생타코",
       description: "멕시코가 그대로! 인생타코입니다",
-      image: "../img/taco.jpg",
+      image: "/img/taco.jpg",
     },
     {
       name: "청년스테이크",
       description:
         "청년스테이크입니다. 저희는 청정호주산 소고기만 사용하여 아주 맛있습니다",
-      image: "../img/stake.jpg",
+      image: "/img/stake.jpg",
     },
     {
       name: "하와이 드링크",
@@ -34,19 +34,19 @@ export default function FoodTruck() {
       name: "맨해튼버거",
       description:
         "뉴욕 맨해튼에서 먹던 맛 그대로. 정통 미국식 수제버거 푸드트럭입니다",
-      image: "../img/burger.jpg",
+      image: "/img/burger.jpg",
     },
     {
       name: "버거",
       description:
         "뉴욕 맨해튼에서 먹던 맛 그대로. 정통 미국식 수제버거 푸드트럭입니다",
-      image: "../img/koko.jpg",
+      image: "/img/koko.jpg",
     },
     {
       name: "감자",
       description:
         "뉴욕 맨해튼에서 먹던 맛 그대로. 정통 미국식 수제버거 푸드트럭입니다",
-      image: "../img/hawaii.jpg",
+      image: "/img/hawaii.jpg",
     },
   ];
   return (
