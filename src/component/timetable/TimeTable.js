fc66479c6a9b5706719087d88f08f5ff45fd1ce2
Antraxmin <antraxmin@naver.com>
2023-05-06 18:01:58 +0900

    [feat] add timetable img

--- src/component/timetable/TimeTable.js
+++ src/component/timetable/TimeTable.js
@@ -2,6 +2,7 @@ import React from "react";
 import style from "./TimeTable.module.css";
 import { useState } from "react";
 import SideDrawer from "../category/SideDrawer";
+import TimeTableNav from "./TimeTableNav";
 
 export default function TimeTable() {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
@@ -20,6 +21,14 @@ export default function TimeTable() {
           />
           <p>공연 시간표</p>
         </header>
+        <main>
+          <div className={style.date}>
+            <TimeTableNav />
+          </div>
+          <div className={style.table}>
+            <img src="./img/timetable/day1.png" />
+          </div>
+        </main>
       </div>
     </>
   );
