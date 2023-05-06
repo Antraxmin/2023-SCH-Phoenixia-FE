0409c43bc94fb34c99d14408854357e5dfef9d99
Antraxmin <antraxmin@naver.com>
2023-05-06 23:30:49 +0900

    [feat] timetable styled-components 적용

--- src/component/timetable/TimeTable.js
+++ src/component/timetable/TimeTable.js
@@ -3,33 +3,102 @@ import style from "./TimeTable.module.css";
 import { useState } from "react";
 import SideDrawer from "../category/SideDrawer";
 import TimeTableNav from "./TimeTableNav";
+import firstdayimage from "../../img/timetable/day1.png";
+import seconddayimage from "../../img/timetable/day2.png";
+import thirddayimage from "../../img/timetable/day3.png";
+import styled from "styled-components";
+import appbar from "../../img/common/app_bar_dark.png";
 
 export default function TimeTable() {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+  const [image, setImage] = useState(FirstDayImg);
+
   return (
     <>
       {drawerIsOpen == true ? <SideDrawer /> : null}
-      <div className={style.container}>
-        <header className={style.header}>
-          <img
-            src="./img/app_bar_dark.png"
-            alt=""
-            width={18}
+      <Container>
+        <Header>
+          <AppbarImg
             onClick={() => {
               setDrawerIsOpen(!drawerIsOpen);
             }}
           />
-          <p>공연 시간표</p>
-        </header>
+          <HeaderTitle>공연 시간표</HeaderTitle>
+        </Header>
         <main>
-          <div className={style.date}>
-            <TimeTableNav />
-          </div>
-          <div className={style.table}>
-            <img src="./img/timetable/day1.png" />
-          </div>
+          <DateContainer>
+            <TimeTableNav setImage={setImage} />
+          </DateContainer>
+          <TableContainer>
+            <FirstDayImg />
+          </TableContainer>
         </main>
-      </div>
+      </Container>
     </>
   );
 }
+
+const Container = style.div`
+  width: 100%;
+  height: 100%;
+  max-width: 390px;
+  margin: 0 auto;
+  background-color: white;
+  overflow-y: scroll;
+`;
+
+const Header = styled.div`
+  width: 100%;
+  max-width: 390px;
+  padding-left: 20px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+`;
+
+const AppbarImg = styled.img.attrs({
+  src: `${appbar}`,
+})`
+  width: 18px;
+`;
+
+const HeaderTitle = styled.p`
+  padding-left: 30px;
+  font-size: 20px;
+  font-family: "GongGothicMedium";
+  padding-top: 5px;
+`;
+
+const DateContainer = styled.div`
+  height: 90px;
+  display: flex;
+  justify-content: center;
+`;
+
+const TableContainer = styled.div`
+  margin-top: 10px;
+  height: 100vh;
+  justify-content: center;
+  padding: 0px 20px 0px 20px;
+`;
+
+const FirstDayImg = styled.img.attrs({
+  src: `${firstdayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
+`;
+
+const SecondDayImg = styled.img.attrs({
+  src: `${seconddayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
+`;
+
+const ThirdDayImg = styled.img.attrs({
+  src: `${thirddayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
+`;
--- src/component/timetable/TimeTableNav.js
+++ src/component/timetable/TimeTableNav.js
@@ -1,6 +1,9 @@
 import styled from "styled-components";
+import firstdayimage from "../../img/timetable/day1.png";
+import seconddayimage from "../../img/timetable/day2.png";
+import thirddayimage from "../../img/timetable/day3.png";
 
-export default function TimeTableNav() {
+export default function TimeTableNav({ setImage }) {
   const date = [
     {
       day: "9일",
@@ -18,9 +21,24 @@ export default function TimeTableNav() {
   return (
     <>
       <Container>
-        <Content date={date[0]} />
-        <Content date={date[1]} />
-        <Content date={date[2]} />
+        <Content
+          date={date[0]}
+          onclick={() => {
+            setImage(FirstDayImg);
+          }}
+        />
+        <Content
+          date={date[1]}
+          onclick={() => {
+            setImage(SecondDayImg);
+          }}
+        />
+        <Content
+          date={date[2]}
+          onclick={() => {
+            setImage(ThirdDayImg);
+          }}
+        />
       </Container>
     </>
   );
@@ -69,4 +87,25 @@ const Date = styled.div`
 
 const Day = styled.div`
   font-size: 13px;
+`;
+
+const FirstDayImg = styled.img.attrs({
+  src: `${firstdayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
+`;
+
+const SecondDayImg = styled.img.attrs({
+  src: `${seconddayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
+`;
+
+const ThirdDayImg = styled.img.attrs({
+  src: `${thirddayimage}`,
+})`
+  width: 100%;
+  border-radius: 15px;
 `;
