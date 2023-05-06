aa4000dbe76fdb4f369730ba9b5836680a24ed64
Antraxmin <antraxmin@naver.com>
2023-05-06 22:14:23 +0900

    [modify] timetable change to styled-components

--- src/component/timetable/TimeTableNav.js
+++ src/component/timetable/TimeTableNav.js
@@ -1,22 +1,72 @@
-import style from "./TimeTableNav.module.css";
+import styled from "styled-components";
 
 export default function TimeTableNav() {
+  const date = [
+    {
+      day: "9일",
+      date: "화요일",
+    },
+    {
+      day: "10일",
+      date: "수요일",
+    },
+    {
+      day: "11일",
+      date: "목요일",
+    },
+  ];
   return (
     <>
-      <div className={style.container}>
-        <div className={style.content}>
-          <div className={style.date}>9일</div>
-          <div className={style.day}>화요일</div>
-        </div>
-        <div className={style.content}>
-          <div className={style.date}>10일</div>
-          <div className={style.day}>수요일</div>
-        </div>
-        <div className={style.content}>
-          <div className={style.date}>11일</div>
-          <div className={style.day}>목요일</div>
-        </div>
-      </div>
+      <Container>
+        <Content date={date[0]} />
+        <Content date={date[1]} />
+        <Content date={date[2]} />
+      </Container>
     </>
   );
 }
+
+export function Content({ date }) {
+  return (
+    <Today>
+      <Date>{date.day}</Date>
+      <Day>{date.date}</Day>
+    </Today>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  margin-top: 5px;
+  margin-bottom: 5px;
+`;
+
+const Today = styled.div`
+  border-bottom: 3px solid #888888;
+  margin: 7px;
+  padding: 10px 10px 0px 10px;
+  display: flex;
+  flex-direction: column;
+  font-family: "GongGothicMedium";
+  color: #888888;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    color: #000066;
+    cursor: pointer;
+    border-bottom: 3px solid #000066;
+  }
+`;
+
+const Date = styled.div`
+  font-size: 22px;
+  display: inline-block;
+  margin-bottom: 5px;
+  display: flex;
+  justify-content: center;
+`;
+
+const Day = styled.div`
+  font-size: 13px;
+`;
