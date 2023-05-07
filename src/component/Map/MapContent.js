2ce2bb31cc83ddbbeeb4fb4afdaaaa33601713b4
Antraxmin <antraxmin@naver.com>
2023-05-07 17:08:32 +0900

    [feat] 축제 지도 페이지 styled-components 적용

--- src/component/Map/MapContent.js
+++ src/component/Map/MapContent.js
@@ -1,5 +1,4 @@
 import React from "react";
-import style from "./MapContent.module.css";
 import { useState } from "react";
 import SideDrawer from "../category/SideDrawer";
 import MapSection from "./MapSection";
@@ -11,26 +10,55 @@ export default function MapContent() {
   return (
     <>
       {drawerIsOpen == true ? <SideDrawer /> : null}
-      <div className={style.container}>
-        <header className={style.header}>
+      <Container>
+        <Header>
           <AppbarImg
             onClick={() => {
               setDrawerIsOpen(!drawerIsOpen);
             }}
           />
-          <p>축제 지도</p>
-        </header>
-        <main className={style.main}>
+          <HeaderTitle>축제 지도</HeaderTitle>
+        </Header>
+        <Main>
           <MapSection />
-        </main>
-      </div>
+        </Main>
+      </Container>
     </>
   );
 }
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  max-width: 390px;
+  margin: 0 auto;
+  background-color: white;
+  overflow-y: hidden;
+`;
+
+const Header = styled.header`
+  width: 100%;
+  max-width: 390px;
+  padding-left: 20px;
+  height: 70px;
+  display: flex;
+  align-items: center;
+`;
+
+const HeaderTitle = styled.header`
+  padding-left: 30px;
+  font-size: 20px;
+  font-family: "GongGothicMedium";
+  padding-top: 5px;
+`;
 
 const AppbarImg = styled.img.attrs({
   src: `${appbar}`,
 })`
   width: 18px;
   cursor: pointer;
+`;
+
+const Main = styled.main`
+  height: 100%;
 `;
