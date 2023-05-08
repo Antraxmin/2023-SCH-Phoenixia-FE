ce7eed5597499b0ab888c6a6198ea270d8fb0689
Antraxmin <antraxmin@naver.com>
2023-05-08 15:25:18 +0900

    [feat] about us 페이지 구현

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -2,6 +2,9 @@ import React from "react";
 import style from "./About.module.css";
 import { useState } from "react";
 import SideDrawer from "../category/SideDrawer";
+import appbar from "../../img/common/app_bar_dark.png";
+import styled from "styled-components";
+import aboutImage from "../../img/common/about.png";
 
 export default function About() {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
@@ -10,17 +13,31 @@ export default function About() {
       {drawerIsOpen == true ? <SideDrawer /> : null}
       <div className={style.container}>
         <header className={style.header}>
-          <img
-            src="./img/app_bar_dark.png"
-            alt=""
-            width={18}
+          <AppbarImg
             onClick={() => {
               setDrawerIsOpen(!drawerIsOpen);
             }}
           />
           <p>About us</p>
         </header>
+        <main>
+          <AboutImg />
+        </main>
       </div>
     </>
   );
 }
+
+const AppbarImg = styled.img.attrs({
+  src: `${appbar}`,
+})`
+  width: 18px;
+  cursor: pointer;
+`;
+
+const AboutImg = styled.img.attrs({
+  src: `${aboutImage}`,
+})`
+  width: 100%;
+  cursor: pointer;
+`;
